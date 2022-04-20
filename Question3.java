aa3cdefa1bf4c32cb55356694b3ec3f040d2d541
s10205229 <94030065+s10205229@users.noreply.github.com>
2022-04-21 02:49:30 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,19 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+
+    /**inputting data & setting variable**/
+    System.out.println("Please enter an integer:");
+    long integer = in.nextInt();
+    int i = 1;
+
+    /**to run the first 5 multiplication**/
+    while(i <= 5){
+      integer = integer*integer;
+      i += 1;
+      
+      /**to display integer**/
+      System.out.println(integer);
+    }
   }
 }
