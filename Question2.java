b29d51d5ff960d73cf8ca08d9db6ccdd18bf27a5
s10205229 <94030065+s10205229@users.noreply.github.com>
2022-04-21 02:27:27 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -9,8 +9,23 @@ public class Question2
      * 
      * Hint: in.nextDouble() to read double
      */
-     
+    
+    /**scanner is a class in java. util package used for obtaining the input of 
+    the primitive types like int, double, etc. and strings.*/
     Scanner in = new Scanner(System.in);
+    
+    /**prompting input & setting variable**/
+    System.out.println("Please enter your height(m): ");
     double height = in.nextDouble();
+
+    System.out.println("Please enter your weight(kg): ");
+    double weight = in.nextDouble();
+    
+    /**calculation**/
+    double bmi = weight/(height*height);
+    
+    /**displaying BMI**/
+    System.out.println(bmi);
+    
   }
 }
