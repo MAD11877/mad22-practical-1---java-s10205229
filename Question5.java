eccfe613f3d223b6fc4c85bb90fcd4df539e2215
s10205229 <94030065+s10205229@users.noreply.github.com>
2022-04-22 01:39:20 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -27,6 +27,55 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+
+    /**prompting input & setting variable**/
+    System.out.println("Please enter an integer:");
+    int num = in.nextInt();
+    int[] b = new int[num];
+    int i = 0;
+
+    System.out.println("Please enter a set of integer:");
+
+    /**while loop is used to cpature all the inputs**/
+    while (i < num){
+
+      int tempnum = in.nextInt();
+
+      b[i] = tempnum;
+
+      i += 1;
+    }
+
+    int maxint = 0;
+    int maxoccurrence = 0;
+    
+    /**looking for integer with the max. occurrence**/
+    for (int k = 0; k < b.length; k++){
+
+      int tempnum = b[k];
+      int tempoccurrence = 0;
+
+      /**doing comparesion**/
+      for (int l = 0; l < b.length; l++){
+        
+        if (tempnum == b[l]){
+  
+          tempoccurrence += 1;
+          
+        }
+      }
+
+       /**doing overwritting**/
+      if (maxoccurrence < tempoccurrence){
+
+        maxoccurrence = tempoccurrence;
+        maxint = b[k];
+        
+      }
+    }
+
+    /**displaying**/
+    System.out.println("Integer: " + Integer.toString(maxint));
     
   }
 }
